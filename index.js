371997bf2ffc739a0ae832bfe62bc98ec1401506
vivianna ramirez <vivrami2001@gmail.com>
2023-04-08 13:08:03 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+const headquarters= 42
+return Math.abs(blocks - headquarters);
+}
+function distanceFromHqInFeet(blocks) {
+    return (distanceFromHqInBlocks(blocks) * 264);
+}
+function distanceTravelledInFeet (start, destination) {
+    let feet = Math. abs (destination-start) ;
+return feet*264;
+}
+function calculatesFarePrice(start, destination) {
+    let distance= distanceTravelledInFeet(start, destination)
+    if (distance <= 400){
+        return 0;
+    }else if (distance >= 400 && distance <=2000){
+       return Math.abs(distance - 400)*0.02;
+    }else if (distance >= 2000 && distance <= 2500){
+        return 25;
+    }else if (distance > 2500){
+        return 'cannot travel that far';
+    }
+}
